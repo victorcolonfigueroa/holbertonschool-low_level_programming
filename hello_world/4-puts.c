9c98df448b22044e67ce1ebe0c045fe66238d09a
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-02-19 22:31:51 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,6 @@
 #!/bin/bash
-gcc puts("\"Programming is like building a multilingual puzzle"); return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
