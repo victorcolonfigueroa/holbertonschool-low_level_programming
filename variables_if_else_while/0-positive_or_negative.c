3e9c17f72ce80ce344b77d14c856d318c9ffb4ab
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-02-20 10:20:08 -0400

    fixed 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,13 +18,13 @@ int main(void)
 	{
 		
 		printf("%d is positive \n" , n);
-	
 	}
-	else (n = 0){
-
-		printf("%d is zero \n", n);
+	else if (n == 0)
+	{
+    	printf("%d is zero \n", n);
 	}
-	else (n < 0){
+	else
+	{
 
 		printf("%d is negative \n", n);
 	}
