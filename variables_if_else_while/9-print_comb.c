d2428fdf140106a07a0d1389e70179aec65f2914
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-02-20 20:11:28 -0400

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		putchar((i % 9) + '0');
-		if (i == 9)
-		putchar(' ');
-		putchar(',');
-		putchar('$');
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
