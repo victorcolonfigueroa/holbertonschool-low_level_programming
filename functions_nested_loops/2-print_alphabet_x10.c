a7558bc8a688fce2d90da3660ce5d0b9d7de8afd
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-02-21 14:28:23 -0400

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+	
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+			
+			c++;
+		}
+		
+		_putchar('\n');
+
+		i++;
+	}
+}
