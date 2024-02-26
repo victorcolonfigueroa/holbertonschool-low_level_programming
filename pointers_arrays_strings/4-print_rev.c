bb5b3ca5f4ad04543a66a9645de3e981770ffee5
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-02-26 11:44:01 -0400

    fixed spacing

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,16 @@ void print_rev(char *s)
 
 	i = 0;
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 	len = i;
 
-	for (j = len -1; j >= 0; j--)
+	for (j = len - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
-	
+
 	_putchar('\n');
 }
