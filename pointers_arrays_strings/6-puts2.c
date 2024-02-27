cfc404f99e00a8d4986596c6250aa4804d89a8e4
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-02-26 22:33:05 -0400

    added 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,11 @@ void puts2(char *s)
 	int a;
 
 	for (a = 0; s[a] != '\0'; a++)
-			{
-			if (a % 2 == 0)
-			{
+	{
+		if (a % 2 == 0)
+		{
 			_putchar(s[a]);
-			}
-			}
-			_putchar('\n');
-			}
+		}
+	}
+	_putchar('\n');
+}
