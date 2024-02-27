87d6fa8e5b50dc7328ef02f18a574bca8fd6ad0d
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-02-26 22:31:30 -0400

    added 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2- prints one char out of 2 of a string
+ * followed by a new line
+ * @s: string to print the char from
+ */
+void puts2(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+			{
+			if (a % 2 == 0)
+			{
+			_putchar(s[a]);
+			}
+			}
+			_putchar('\n');
+			}
