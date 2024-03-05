c1da1a2ff8e210fe79b66cfed23157b31035f1dc
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-03-05 11:51:09 -0400

    fixed betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  *
  * Return: Always o.
  */
-int main(int __attribute__((__unused__)) argc, char* argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf ("%s\n", argv[0]);
-	
-	return 0;
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
