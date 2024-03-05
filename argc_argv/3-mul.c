cba6fa71a87055856464223bbe7979da9c80cafe
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-03-05 12:57:03 -0400

    added files for task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Prints all arguments it receives.
@@ -9,10 +10,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int arg;
+	int num1, num2, prod;
 
-	for (arg = 0; arg < argc; arg++)
-		printf("%s\n", argv[arg]);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
 
 	return (0);
 }
