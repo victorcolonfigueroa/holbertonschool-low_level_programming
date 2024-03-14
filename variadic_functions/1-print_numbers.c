1e9b1736b68f1966ed1dcfbb543cba3d0fbea93e
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-03-14 11:26:55 -0400

    added else case

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,23 +1,26 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
 /**
- * print_numbers - prints numbers
- * @separator: character to use to delimit
- * @n: number of elements to print
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: string to be printed between numbers
+ * @n: num of int
+ * Return: ntg
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list valist;
 	unsigned int i;
-	va_list ap;
 
-	va_start(ap, n);
+	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, int));
-		if (separator != NULL && 1 < n - 1)
-			printf("%s", separator);
+		if (i != (n - 1) && separator != NULL)
+			printf("%d%s", va_arg(valist, unsigned int), separator);
+		else
+			printf("%d", va_arg(valist, unsigned int));
 	}
 	printf("\n");
-	va_end(ap);
+	va_end(valist);
 }
